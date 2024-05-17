0e9a48dcd1de5e2a0f29a5fe10bd4fd6ccb8b433
thanhSEO2803 <tranthanhh2803@gmail.com>
2024-05-18 02:27:16 +0700

    add file

---
+++ Data/TbSinhvien.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BT_QUANLYSINHVIEN.Data;
+
+public partial class TbSinhvien
+{
+    public int Masinhvien { get; set; }
+
+    public string Tensinhvien { get; set; } = null!;
+
+    public DateOnly? Ngaysinh { get; set; }
+
+    public string? Gioitinh { get; set; }
+
+    public string Diachi { get; set; } = null!;
+
+    public string? Email { get; set; }
+
+    public string? Malop { get; set; }
+
+    public virtual TbLopsinhhoat? MalopNavigation { get; set; }
+}
---
+++ Data/TbTruong.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BT_QUANLYSINHVIEN.Data;
+
+public partial class TbTruong
+{
+    public string Matruong { get; set; } = null!;
+
+    public string Tentruong { get; set; } = null!;
+
+    public string Diachi { get; set; } = null!;
+
+    public virtual ICollection<TbKhoa> TbKhoas { get; set; } = new List<TbKhoa>();
+}
