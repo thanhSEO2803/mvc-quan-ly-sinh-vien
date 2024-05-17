b5529def0402aa91aa31db88de4d0c56f1af37bf
thanhSEO2803 <tranthanhh2803@gmail.com>
2024-05-18 02:53:24 +0700

    add file

---
+++ Data/TbLophocphan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BT_QUANLYSINHVIEN.Data;
+
+public partial class TbLophocphan
+{
+    public string Mahocphan { get; set; } = null!;
+
+    public string Tenhocphan { get; set; } = null!;
+
+    public int Soluong { get; set; }
+
+    public string? Magiaovien { get; set; }
+
+    public virtual TbGiaovien? MagiaovienNavigation { get; set; }
+}
